1e8f50282b28d34113628051708174173930034f
limon27121 <moontaser009@gmail.com>
2024-05-13 18:26:50 -0700

    part 4 lecture code is added with comment

---
+++ cypress/e2e/Getting started with Cypress Test Automation/Cypress_Locators.cy.js
@@ -0,0 +1,31 @@
+
+///<reference types="Cypress" />
+describe("visit the site",()=>{
+    beforeEach(() => {
+        // Visit the webpage where your input field is
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+      })
+    it("css locators",()=>{
+        cy.get(".brand.greenLogo").contains("GREEN")
+        cy.get(".redLogo").contains("KART")
+    })
+
+    it('Checks if the placeholder text is correct', () => {
+        // Check if the placeholder text is correct
+        cy.get('.search-keyword')  // Find the input field by its class name
+          .invoke('attr', 'placeholder') // Get the placeholder text
+          .should('equal', 'Search for Vegetables and Fruits'); // Make sure it's what we expect
+      });
+
+      it("check length after write in search-box",()=>{
+        cy.get(".search-keyword").type("ca")
+        cy.wait(1000)
+        cy.get(".product").should("have.length",5)
+      })
+
+      it.only("test only visible part of the section",()=>{
+        cy.get(".search-keyword").type("ca")
+        cy.wait(1000)
+        cy.get(".product:visible").should("have.length",4)
+      })
+})
